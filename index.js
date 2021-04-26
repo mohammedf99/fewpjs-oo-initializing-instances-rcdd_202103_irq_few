1b24f5eec11e3e0b5d5afedf779498cc3a389fbd
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-26 13:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ class Lunch {
   }
   
 }
+
+class Dinner {
+  
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+  
+}
